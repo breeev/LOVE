3beb3af2517adde88d4f43d0f018c786c5533c47
BrevalF <breval.ferrari@gmail.com>
2022-07-26 00:07:54 +0200

    Complete! Only the file management left

--- lov.py
+++ lov.py
@@ -1,7 +1,10 @@
 from mido import Message,MidiFile,MidiTrack,MetaMessage,bpm2tempo
-def t(x:int)->int:return int(1920/x)
+def incr():
+    global i,char
+    i+=1
+    char=st[i]
 mid=MidiFile()
-st="200@1l8o4cccde,d,ceddl1c@2l2o3c.<g."
+st="240@1l8o4(6c)l4de,d,ceddl6cdcc,,@2l2o3c.<g."
 notes="cCdDefFgGaAb"
 i=0
 ddump=""
@@ -11,30 +14,24 @@ while i<len(st):
         if ddump:
             mid.tracks.append(MidiTrack([MetaMessage('midi_port',port=0),MetaMessage('set_tempo',tempo=bpm2tempo(int(ddump)))]))
             ddump=""
-        i+=1
-        char=st[i]
+        incr()
         track=MidiTrack()
         mid.tracks.append(track)
         track.append(MetaMessage('midi_port',port=int(char,16)))
-        l=4
-        o=4
-        v=64
+        l,o,v=4,4,64# LOV <3
         s=0
         while char!="@" and i<len(st)-1:
-            i+=1
-            char=st[i]
+            incr()
             if char=='<':o-=1
             elif char=='>':o+=1
             elif char=='+':v+=1
             elif char=='-':v-=1
             elif char=='l':
                 d=''
-                i+=1
-                char=st[i]
+                incr()
                 while char.isdigit():
                     d+=char
-                    i+=1
-                    char=st[i]
+                    incr()
                 i-=1
                 l=int(d)
             elif char=='o':
@@ -43,14 +40,12 @@ while i<len(st):
             elif char=='v':
                 i+=1
                 v=int(st[i],16)
-            elif char=='"':
+            elif char=="'":
                 d=''
-                i+=1
-                char=st[i]
-                while char!='"':
+                incr()
+                while char!="'":
                     d+=char
-                    i+=1
-                    char=st[i]
+                    incr()
                 track.append(MetaMessage('lyrics',text=d))
             elif char in notes:
                 n=notes.index(char)+12*(o+1)
@@ -58,10 +53,26 @@ while i<len(st):
                 if s:s=0
             elif char==',':track[-1].time+=int(1920/l)
             elif char=='.':s+=1920/l
+            elif char=='(':
+                ids=i
+                r=''
+                incr()
+                while char.isdigit():
+                    r+=char
+                    incr()
+                ida=i
+                level=1
+                while level:
+                    incr()
+                    if char==')':level-=1
+                    elif char=='(':level+=1
+                st=st[:ids]+(int(r) if r else 2)*st[ida:i]+st[i+1:]
+                i=ids-1
         i-=1
     elif char.isdigit:ddump+=char
     i+=1
 mid.save('new_song.mid')
 print(mid.tracks)
+
 from os import system
 system(r'D:\MidiSheetMusic-2.6.2.exe C:\Users\breva\LOV\new_song.mid')
