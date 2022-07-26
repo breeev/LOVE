fac00b89b400da57033d625e54ef18de2767448d
BrevalF <breval.ferrari@gmail.com>
2022-07-26 11:23:48 +0200

    Verbose + length counter in beats

--- lov.py
+++ lov.py
@@ -1,10 +1,10 @@
-from mido import Message,MidiFile,MidiTrack,MetaMessage,bpm2tempo
+from mido import Message,MidiFile,MidiTrack,MetaMessage,bpm2tempo,tick2second
 def incr():
     global i,char
     i+=1
     char=st[i]
 mid=MidiFile()
-st="240@1l8o4((3c+))l4de,d,+c-e+d-dl6cv1dvFcc,,@2l2o3c.<g."
+st="My First Song\nt240Tinky Winky... La-la@1l8o4((3c+))l4de,d,+c-e+d-dl6cv1dvFcc,,@2l2o3c.<g."
 notes="cCdDefFgGaAb"
 i=0
 ddump=""
@@ -12,12 +12,15 @@ while i<len(st):
     char=st[i]
     if char=="@":
         if ddump:
-            mid.tracks.append(MidiTrack([MetaMessage('midi_port',port=0),MetaMessage('set_tempo',tempo=bpm2tempo(int(ddump)))]))
+            print('{:>11}{:>5}'.format('BPM: ',ddump))
+            t=bpm2tempo(int(ddump))
+            mid.tracks.append(MidiTrack([MetaMessage('midi_port',port=0),MetaMessage('set_tempo',tempo=t)]))
             ddump=""
         incr()
         track=MidiTrack()
         mid.tracks.append(track)
         track.append(MetaMessage('midi_port',port=int(char,16)))
+        ch=char
         l,o,v=4,4,64# LOV <3
         s=0
         while char!="@" and i<len(st)-1:
@@ -69,11 +72,17 @@ while i<len(st):
                 i-=1
                 st=st[:ids]+(int(r) if r else 2)*st[ida:i]+st[i+1:]
                 i=ids-1
+        print('Channel {}: {:>5} beats'.format(ch,(tick2second(sum(m.time for m in track),480,t)*1000000)/t))
         i-=1
-    elif char.isdigit:ddump+=char
+    elif char=='t':
+        incr()
+        while char.isdigit():
+            ddump+=char
+            incr()
+        i-=1
     i+=1
-mid.save('new_song.mid')
-print(mid.tracks)
+name=st.splitlines()[0]+'.mid'
+mid.save(name)
 
 from os import system
-system(r'D:\MidiSheetMusic-2.6.2.exe C:\Users\breva\LOV\new_song.mid')
+system(r'D:\MidiSheetMusic-2.6.2.exe C:\Users\breva\LOV\\"'+name+'"')
