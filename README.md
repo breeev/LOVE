5ed20389cad337337de695883d34fbba82441e14
BrevalF <breval.ferrari@gmail.com>
2022-07-26 19:26:56 +0200

    Replace icons with emojis

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # L.O.V.
-My advanced MML inspired golfing language (in progress): Lengh, Octave, Velocity / Volume  
+My advanced MML inspired golfing language (in progress): Lengh, Octave, Velocity / Volume
 
 ## What?
 LOV. This is a lighter Blip interpreter designed to send midi messages to a file or a local (virtual) loopback MIDI device like <a href="https://www.tobias-erichsen.de/software/loopmidi.html">LoopMIDI</a>. These messages can then be used by a sound system built in Python, Pure-Data or just any MIDI player or editor to play, shape sounds and instruments, or edit songs.
--- ide.py
+++ ide.py
@@ -1,10 +1,33 @@
-from tkinter import Tk as Window,Text,Frame
+from tkinter import Tk as Window,Text,Frame,Button,PhotoImage
+from tkinter.font import Font
+from cairosvg import svg2png
+
+backgroundColor='black'
+foregroundColor='lime'
+chars={
+    'exit':'\u274E',
+    'new':'\u2747',
+    'load':'\u2934',
+    'save':'\u2935',
+    'run':'\u25B6',
+    'settings':'\u2699'
+}
+btns={}
+
+def ex():root.destroy()
+def new():pass
+def load():pass
+def save():pass
+def run():pass
+def settings():pass
+fcts=[ex,new,load,save,run,settings]
+
 root=Window()
-print(root.winfo_screenheight())
-print(root.winfo_screenwidth())
+sideFont=Font(family='Arial',size=16)
 root.wm_attributes('-fullscreen', 'True')
-border=Frame(root,width=50,bg='black').pack(fill='y',side='left')
-status=Frame(root,height=20,bg='blue').pack(fill='x',side='bottom')
-text=Text(root).pack(expand=True, fill='both')
+side=Frame(root,width=50,bg=backgroundColor,relief='ridge',bd=1).pack(fill='y',side='left')
+status=Frame(root,height=20,bg=backgroundColor,relief='ridge',bd=1).pack(fill='x',side='bottom')
+text=Text(root,bg=backgroundColor).pack(expand=True, fill='both')
+for i,b in enumerate(chars):btns[b]=Button(side,bg=backgroundColor,fg=foregroundColor,activebackground=foregroundColor,activeforeground=backgroundColor,text=chars[b],font=sideFont,relief='flat',command=fcts[i]).place(x=1,y=10+50*i)
 root.focus_force()
 root.mainloop()
